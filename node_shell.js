ec3073ad8914be6fba555b982cff17701ba8f74f
tybfm <112153969+tybfm@users.noreply.github.com>
2023-01-02 01:41:56 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'74b7895d-8142-4b16-ad5e-920b96728b95'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
